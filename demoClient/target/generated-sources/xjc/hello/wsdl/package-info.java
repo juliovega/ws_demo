e273b248ee03d299d5c95da7d1b4456189db8f57
juliovega <fc900921@hotmail.com>
2016-11-29 19:13:34 -0600

    deleting target

--- demoClient/target/generated-sources/xjc/hello/wsdl/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2016.11.29 a las 06:52:30 PM CST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.cdyne.com/WeatherWS/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package hello.wsdl;
